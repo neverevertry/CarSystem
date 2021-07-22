ef5198ebef7ea10e9da6536bcef0f3892477ec68
neverevertry <ofkpypa@yandex.ru>
2021-07-22 15:10:27 +0300

    AddFile

---
+++ Регистрация машин/ReadIntoBaseToListAvehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Регистрация_машин
+{
+    class ReadIntoBaseToListAvehicle
+    {
+        public void ReadIntoBaseToListHijackedCar(CarCheckingSystem ccs)
+        {
+            Console.WriteLine("Укажите путь к файлу");
+            string path = Console.ReadLine();
+            try
+            {
+                StreamReader sr = new StreamReader(path);
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    ccs.NumStolenCars.Add(line);
+                }
+                Console.WriteLine("Successful import");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Not found file");
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
